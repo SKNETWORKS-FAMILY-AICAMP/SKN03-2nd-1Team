ca85737b0709c27de6cd5063e76c4b27dec59bf0
Lerini98 <hyerin7259@gmail.com>
2024-08-16 01:53:23 +0900

    feat : button & db connect

--- UUJJ/city/views.py
+++ UUJJ/city/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
-def city(request):
-    return render(request, 'city/city.html')
+
+def city_view(request):
+    return render(request,"city/city.html")
+
