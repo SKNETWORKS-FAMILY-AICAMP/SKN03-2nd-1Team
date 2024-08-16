e01559de83e6100dc076cebde584cf1d7157d312
singas0ng <031212sms@gmail.com>
2024-08-17 01:08:00 +0900

    add rank

--- UUJJ/rank/urls.py
+++ UUJJ/rank/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import rank
 
 urlpatterns = [
-    path('', rank),
+    path('', rank, name='rank_spa'),  # 기본 페이지
+    path('<str:category>/', rank, name='rank_category'),  # 다른 카테고리 페이지
 ]
